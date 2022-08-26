20ebce19f695aa6238aedd8c6002ae925b3f05b9
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2022-08-26 10:52:58 -0300

    feat: post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,38 @@
-export function Post(props) {
+import styles from "./Post.module.css";
+
+export function Post() {
   return (
-    <div>
-      <strong>{props.author}</strong>
-      <p>{props.content}</p>
-    </div>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img 
+            className={styles.avatar}
+            src="https://github.com/nicolysampaio.png" 
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Nicoly Sampaio</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time 
+          title="09 de junho às 22h35" 
+          dateTime="2022-06-09 22:36:30"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉<a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{" "}
+          <a href="">#nlw</a>{" "} 
+          <a href="">#rocketseat</a>{" "}
+        </p>
+      </div>
+    </article>
   )
 }
